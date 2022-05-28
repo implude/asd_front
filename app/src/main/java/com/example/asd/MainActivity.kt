d78b5e998d5722fa7097ac3bce50e261eddb20da
eks05 <eks0220@naver.com>
2022-05-28 15:17:47 +0900

    'loginpage'

--- app/src/main/java/com/example/asd/MainActivity.kt
+++ app/src/main/java/com/example/asd/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.asd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
+
+        val togo_find_id = findViewById<TextView>(R.id.find_id)
+        val togo_find_pw = findViewById<TextView>(R.id.find_pw)
+        val togo_register = findViewById<TextView>(R.id.register)
+
     }
 }
 
