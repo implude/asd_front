7add89a6ac35eb49bf7dc2a2e0d5126423954d81
seonghun <kimsh20502@gmail.com>
2022-05-28 23:46:53 +0900

    register page To do : inputtype

--- app/src/main/java/com/example/asd/Login.kt
+++ app/src/main/java/com/example/asd/Login.kt
@@ -1,5 +1,6 @@
 package com.example.asd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,7 +8,7 @@ import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
@@ -16,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         val togo_find_pw = findViewById<TextView>(R.id.find_pw)
         val togo_register = findViewById<TextView>(R.id.register)
 
+//      회원가입 페이지 이동
+        togo_register.setOnClickListener {
+            val go_register = Intent(this, Register::class.java)
+            startActivity(go_register)
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/asd/Register.kt
@@ -0,0 +1,11 @@
+package com.example.asd
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Register :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+    }
+}
