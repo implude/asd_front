c7d2c018e4f86ea24d21962f2b70eb47007612bf
eks05 <eks0220@naver.com>
2022-07-13 21:48:35 +0900

    hi

--- app/src/main/java/com/example/asd/Login.kt
+++ app/src/main/java/com/example/asd/Login.kt
@@ -2,11 +2,11 @@ package com.example.asd
 
 import User_identification_router
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import retrofit2.Retrofit
@@ -21,15 +21,20 @@ class Login : AppCompatActivity() {
         //우리 서버 주소
 //        val url = null
 
-        // retrofit2 server connection part. login, register etc ..
+//         retrofit2 server connection part. login, register etc ..
 //        val retrofit = Retrofit.Builder().baseUrl(url.toString())
 //            .addConverterFactory(GsonConverterFactory.create()).build();
 //        val service = retrofit.create(User_identification_router::class.java);
 
 
         // check
-        findViewById<Button>(R.id.login_button).setOnClickListener{
+        findViewById<TextView>(R.id.login_button).setOnClickListener{
             val intent = Intent(this@Login, Main::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<TextView>(R.id.login_findId).setOnClickListener{
+            val intent = Intent(this@Login, find_id::class.java)
             startActivity(intent)
         }
     }
