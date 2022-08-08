814a29abf59f844bb45c79b36590630c5a61723a
eks05 <65056583+eks05@users.noreply.github.com>
2022-08-08 22:48:30 +0900

    'hi'

--- app/src/main/java/com/example/asd/Calendar.kt
+++ app/src/main/java/com/example/asd/Calendar.kt
@@ -1,6 +1,5 @@
 package com.example.asd
 
-import TodoViewModel
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
@@ -9,20 +8,12 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.asd.databinding.CalendarBinding
-import com.example.asd.dto.Todo
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class Calendar : AppCompatActivity() {
-    lateinit var binding: CalendarBinding
-    lateinit var todoViewModel: TodoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = CalendarBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+        setContentView(R.layout.calendar)
 
         val navigationbar_book = findViewById<ImageButton>(R.id.navigation_bar_book)
         val navigationbar_home = findViewById<ImageButton>(R.id.navigation_bar_home)
@@ -35,34 +26,13 @@ class Calendar : AppCompatActivity() {
         navigationbar_home.setOnClickListener {
             val intent = Intent(this@Calendar, Main::class.java)
             startActivity(intent)
-            overridePendingTransition(R.anim.slide_right_enter,R.anim.slide_right_exit)
+            overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit)
         }
         navigationbar_info.setOnClickListener {
             val intent = Intent(this@Calendar, setting::class.java)
             startActivity(intent)
-            overridePendingTransition(R.anim.slide_right_enter,R.anim.slide_right_exit)
+            overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit)
         }
 
-        binding.recycleradd.setOnClickListener {
-            val intent = Intent(this, EditTodoActivity::class.java).apply {
-                putExtra("type", "ADD")
-            }
-            requestActivity.launch(intent)
-        }
-    }
-    private val requestActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == RESULT_OK) {
-            val todo = it.data?.getSerializableExtra("todo") as Todo
-
-            when(it.data?.getIntExtra("flag", -1)) {
-                0 -> {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        todoViewModel.insert(todo)
-                    }
-                    Toast.makeText(this, "추가되었습니다.", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
     }
 }
-
